3fc0e2d441fded7418a4357418a67bbf9ac17b43
bcooperstl <coop999@gmail.com>
2020-05-20 14:14:23 -0500

    Added functions

--- chapter-2/hunt_the_wumpus.py
+++ chapter-2/hunt_the_wumpus.py
@@ -2,41 +2,86 @@
 
 from random import choice
 
-#cave_numbers = range(1,21)
+def create_tunnel(cave_from, cave_to):
+    """ Create a tunner from cave_from and cave_to """
+    caves[cave_from].append(cave_to)
+    caves[cave_to].append(cave_from)
+
+def visit_cave(cave_number):
+    """ Mark a cave as visited """
+    visited_caves.append(cave_number)
+    unvisited_caves.remove(cave_number)
+
+def choose_cave(cave_list):
+    """Pick a cave from a list, provided
+    that the cave has less than 3 tunnels."""
+    cave_number = choice(cave_list)
+    while len(caves[cave_number]) >= 3:
+        cave_number = choice(cave_list)
+    return cave_number
+
+def print_caves():
+    """ Print out the current cave structure """
+    for number in cave_numbers:
+        print(number, ":", caves[number])
+    print('----------')
+
+def setup_caves(cave_numbers):
+    """ Create the starting list of caves """
+    caves = []
+    for cave in cave_numbers:
+        caves.append([])
+    return caves
+
+def link_caves():
+    """ Make sure all of the caves are connected
+    with two-way tunnels """
+    while unvisited_caves != []:
+        this_cave = choose_cave(visited_caves)
+        next_cave = choose_cave(unvisited_caves)
+        create_tunnel(this_cave, next_cave)
+        visit_cave(next_cave)
+
+def finish_caves():
+    """ Link the rest of the caves with
+    one-way tunnels"""
+    for cave in cave_numbers:
+        while len(caves[cave]) < 3:
+            passage_to = choose_cave(cave_numbers)
+            caves[cave].append(passage_to)
+
+def print_location(player_location):
+    """ Tell the player about where they are """
+    print("You are in cave", player_location)
+    print("From here, you can see caves:", caves[player_location])
+    if wumpus_location in caves[player_location]:
+        print("I smell a wumpus!")
+    if wumpus_friend_location in caves[player_location]:
+        print("I smell another wumpus!")
+
+def get_next_location():
+    """ Get teh player's next location """
+    print("Which cave next?")
+    player_input = input("> ")
+    if (not player_input.isdigit() or
+        int(player_input) not in caves[player_location]):
+        print(player_input+"?")
+        print("That's not a direction that I can see!")
+        return None
+    else:
+        return int(player_input)
+
 cave_numbers = range(0,20)
-caves = []
-for i in cave_numbers:
-    caves.append([])
-
 unvisited_caves = list(range(0,20))
-visited_caves = [0]
-unvisited_caves.remove(0)
+visited_caves = []
+caves = setup_caves(cave_numbers)
 
-while unvisited_caves != []:
-    i = choice(visited_caves)
-    if len(caves[i]) >= 3:
-        continue
-    
-    next_cave = choice(unvisited_caves)
-    caves[i].append(next_cave)
-    caves[next_cave].append(i)
-    visited_caves.append(next_cave)
-    unvisited_caves.remove(next_cave)
-    
-    for number in cave_numbers:
-        print(number, ":", caves[number])
-    print('----------')
-
-for i in cave_numbers:
-    while (len(caves[i]) < 3):
-        passage_to = choice(cave_numbers)
-        caves[i].append(passage_to)
-        
-    for number in cave_numbers:
-        print(number, ":", caves[number])
-    print('----------')
-
-print(caves)
+visit_cave(0)
+print_caves()
+link_caves()
+print_caves()
+finish_caves()
+print_caves()
 
 wumpus_location = choice(cave_numbers)
 wumpus_friend_location = choice(cave_numbers)
@@ -50,28 +95,17 @@ while (player_location == wumpus_location or
     player_location = choice(cave_numbers)
 
 print("Welcome to Hunt the Wumpus!")
-print("You can see", len(cave_numbers), "caves")
 print("To play, just type the number")
 print("of the cave you wish to enter next")
 
 while True:
-    print("You are in cave", player_location)
-    print("From here, you can see caves:", caves[player_location])
-    if wumpus_location in caves[player_location]:
-        print("I smell a wumpus!")
-    if wumpus_friend_location in caves[player_location]:
-        print("I smell another wumpus!")
-    
-    print("Which cave next?")
-    player_input = input("> ")
-    if (not player_input.isdigit() or
-        int(player_input) not in caves[player_location]):
-        print(player_input," is not a cave!")
-    else:
-        player_location = int(player_input)
-        if (player_location == wumpus_location):
-            print("Aargh! You got eaten by a wumpus!")
-            break
-        if (player_location == wumpus_friend_location):
-            print("Aargh! You got eaten by the wumpus' friend!")
-            break
+    print_location(player_location)
+    new_location = get_next_location()
+    if new_location is not None:
+        player_location = new_location
+    if player_location == wumpus_location:
+        print("Aargh! You got eaten by a wumpus!")
+        break
+    if player_location == wumpus_friend_location:
+        print("Aargh! You got eaten by the wumpus' friend!")
+        break
