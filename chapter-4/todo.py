d1c64860d3819a110c011676d953e1a892fbbd8e
bcooperstl <coop999@gmail.com>
2020-05-24 13:23:42 -0500

    added boilerplate main loop

--- chapter-4/todo.py
+++ chapter-4/todo.py
@@ -5,3 +5,22 @@ def create_todo(todos, title, description, level):
             'description' : description,
             'level' : level }
     todos.append(todo)
+
+def get_input(fields):
+    user_input = {}
+    for field in fields:
+        user_input[field] = input(field+" > ")
+    return user_input
+
+def main_loop():
+    user_input = ""
+    while 1:
+        print(run_command(user_input))
+        user_input = input("> ")
+        if user_input.lower().startswith("quit"):
+            print("Exiting...")
+            break;
+
+if __name__ == '__main__':
+    main_loop()
+
